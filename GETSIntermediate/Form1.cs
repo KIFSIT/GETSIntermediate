4b42d0d77588295ec9a6f533d4e718b7ae95f120
Administrator <Administrator@DESKTOP-IJJ8HL0>
2022-11-14 14:54:02 +0530

    Second commit

--- GETSIntermediate/Form1.cs
+++ GETSIntermediate/Form1.cs
@@ -15,7 +15,7 @@ namespace GETSIntermediate
 {
     public partial class Form1 : Form
     {
-        //first commit
+        //second commit
         ClientSocketManager m_ServerTerminal;
         internal StreamWriter _errorLog;
         internal StreamWriter _transactionLog;
